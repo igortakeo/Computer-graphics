ae50636e6a177a0cedef6afe3f2c5ebcc5381c62
igortakeo <igor_takeo77@hotmail.com>
2021-05-30 19:13:11 -0300

    Update

--- T1/Headers/FragmentShaderInit.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef FRAGMENTSHADERINIT_H
-#define FRAGMENTSHADERINIT_H
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
-void ShaderSourceFragment(GLuint fragment);
-void CompileFragmentShader(GLuint fragment);
-
-#endif
--- T1/Headers/VertexShaderInit.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef VERTEXSHADERINIT_H
-#define VERTEXSHADERINIT_H
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
-void ShaderSourceVertex(GLuint vertex);
-void CompileVertexShader(GLuint vertex);
-
-#endif
